f5508814c4d1d707db60c061df31e0f2f7a8aac8
Richard Phillips <rickwphillips@users.noreply.github.com>
2023-01-20 11:41:51 -0500

    Recipes Model & Scaffolding + Created recipe.model.ts + Created recipe scaffolding in recipe-detail.component.html & recipe-list.component.html + Added dummy recipe in recipe-list.component.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "src/app/recipes/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('Pumpkin Pie',
+      'A delicious pumpkin pie',
+      'https://www.simplyrecipes.com/thmb/T_J_02rJmbxEPvizYtVlqsIqqSs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Pumpkin-Pie-Lead-6-335f0cd430ba439fb7449b8abfebe45e.jpg'
+      )
+  ];
 
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 }
