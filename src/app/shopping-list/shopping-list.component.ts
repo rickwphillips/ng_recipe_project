aad336e0249ffe1a1dbda5c0e5b871e2301e465b
Richard Phillips <rickwphillips@users.noreply.github.com>
2023-01-20 14:44:49 -0500

    Shopping Model & Scaffolding + Created ingredient.model.ts & updated recipe.model.ts with constructor shorthand + Created the shopping-edit.component.html form + Added the ingredient scaffolding in shopping-list.component.html + Added constructor and OnInit to shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients = [
+    new Ingredient('Sugar', 4, 'cups'),
+    new Ingredient('Pie Crust', 1, 'package'),
+    new Ingredient('Pumpkin', 1, 'whole')
+  ];
 
+  constructor() {
+  }
+  ngOnInit() {
+  }
 }
